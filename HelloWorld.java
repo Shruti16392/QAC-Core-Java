c4f15d556c19ad4cdf8603d93f1d2699a3ecca27
Shruti16392 <Shrushiremath@gmail.com>
2021-12-10 20:19:25 +0530

    Adding 2 java files

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld{
+
+public static void main (String[] args){
+
+	System.out.println("Main method started");
+	System.out.println("Hello QAC people");
+    System.out.println("Main method End");
+
+
+   }
+
+
+}
